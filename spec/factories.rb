e104c6892fc5a4d2a62837e4d26df4edadd27585
dillon spencer <dillonstreetfrontcommercial@gmail.com>
2020-01-29 21:16:59 +0000

    Lesson 11 complete

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :user do
     sequence :email do |n|
-      { "dummyEmail#@gmail.com" }
+       "dummyEmail#{n}@gmail.com" 
     end
     password { "secretPassword" }
     password_confirmation { "secretPassword" }
   end
+end
